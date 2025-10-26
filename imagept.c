dca696b6ad67287837cb023250fde41456743ac6
Mateo Torres <mateot659@gmail.com>
2025-10-26 14:15:53 -0400

    amsdlkasd

--- imagept.c
+++ imagept.c
@@ -77,7 +77,7 @@ void convolute(void* data) {
     ConvoluteData* convData = (ConvoluteData*) data;
     Image* srcImage = convData->srcImage;
     Image* destImage = convData->destImage;
-    Matrix algorithm = convData->algorithm;
+    Matrix algorithm = (convData->algorithm)[0];
     long rank = convData->rank;
 
     localStartRow = rank* srcImage->height / thread_count;
